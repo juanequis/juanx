f4fdef0e884a6d112fae8e7c08222591c07b50f9
Juan Cruz  Avellaneda <juanxavellaneda@gmail.com>
2025-04-16 16:28:26 +0200

    Initial commits setting up nextJS

---
+++ app/home/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "@/page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            SAPEEEE <code>app/page.tsx</code>
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
+}
