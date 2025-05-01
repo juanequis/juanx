13fdc779ecb93ef9c062613e9f64ebed48677a65
Juan Cruz  Avellaneda <juanxavellaneda@gmail.com>
2025-05-01 18:20:12 +0200

    merge

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider as ChakraProvider } from '@/components/ui/provider';
-import { Nav } from "./components/nav";
+import { Nav } from "@/app/[lng]/components/nav";
 import { I18nProvider } from '@/lib/i18n/provider';
 
 const geistSans = Geist({
