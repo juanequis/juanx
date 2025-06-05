020abe7008df880d6aa7e6fd935da888cdc1ecf5
Juan Cruz  Avellaneda <juanxavellaneda@gmail.com>
2025-06-05 16:30:11 +0200

    Try again removing fetchApi from createYoga

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,14 +1,14 @@
 import { createYoga } from "graphql-yoga";
 import { createSchema } from "./schema";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
 const { handleRequest } = createYoga({
   schema: createSchema(),
   graphqlEndpoint: "/api/graphql",
-  fetchAPI: {
-    Request: NextRequest as unknown as typeof Request,
-    Response: Response,
-  },
+  // fetchAPI: {
+  //   Request: NextRequest as unknown as typeof Request,
+  //   Response: Response,
+  // },
 });
 
 export { handleRequest as GET, handleRequest as POST };
