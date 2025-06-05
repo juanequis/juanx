77eeee65431be71ef80544ffca50f0f0a0198a4c
Juan Cruz  Avellaneda <juanxavellaneda@gmail.com>
2025-06-05 16:20:29 +0200

    Fixed TS error

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,11 +1,11 @@
 import { createYoga } from "graphql-yoga"
-import { NextRequest } from "next/server"
+// import { NextRequest } from "next/server"
 import { createSchema } from "./schema"
 
 const { handleRequest } = createYoga({
   schema: createSchema(),
   graphqlEndpoint: "/api/graphql",
-  fetchAPI: { Request: NextRequest },
+  // fetchAPI: { Request: NextRequest },
 })
 
 export { handleRequest as GET, handleRequest as POST }
