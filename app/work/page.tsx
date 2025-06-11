a07d6c59dfcea7f6a91ca4c165b8444bd2bda5ce
juanequis <164242480+juanequis@users.noreply.github.com>
2025-06-11 06:09:48 +0200

    Added work page to the app with downloadable CV (#15)
    * Added work page to the app with downloadable CV
    
    * Applied CR changes
    
    * Removed unnused compoennt

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,15 +1,33 @@
-import styles from "@/app/page.module.css";
-import Link from "next/link";
+import styles from "../page.module.css";
+import localStyles from "./page.module.css";
+import { Heading, Text, Button } from "@chakra-ui/react";
+import { MotionBox } from '../_components/motion';
+import { useTranslations } from 'next-intl';
 
-export default function Home() {
+export default function Work() {
+  const t = useTranslations('work');
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ol>
-          <li>
-            <Link href="/"> Return <code> to index!</code>←</Link>
-          </li>
-        </ol>
+        <article className={styles.intro}>
+          <MotionBox
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+          >
+            <Heading size="2xl" className={styles.title} mb={4}>
+              {t('title')}
+            </Heading>
+            <Text mb={4}>{t('description')}</Text>
+          </MotionBox>
+        </article>
+        <article>
+          <a href="/cv.pdf" download target="_blank" className={localStyles.downloadLink} aria-labelledby="downloadCvButton">
+            <Button id="downloadCvButton" variant="surface">
+              {t('downloadCv', { defaultMessage: 'Download CV' })}
+            </Button>
+          </a>
+        </article>
       </main>
     </div>
   );
